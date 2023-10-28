ee86a920b68451004b0c723b3dac8c4d7ae25d2b
nohgijin <ynda11111@naver.com>
2023-10-28 16:42:14 +0900

    fix: post deploy 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'git@github.com:nohgijin/swimday-client.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
