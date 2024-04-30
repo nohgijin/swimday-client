f919573b6d9cbe05278fa768cc36ffbb0f04c6c2
nohgijin <ynda11111@naver.com>
2024-04-30 20:34:06 +0900

    feat: 칩그룹 커스텀 핸들 이벤트 정의

--- components/ChipGroup/index.tsx
+++ components/ChipGroup/index.tsx
@@ -55,35 +55,54 @@ const FILTERS = [
 ]
 
 function AChipGroup() {
-  const [sortValue, setSortValue] = useState('new')
-  const [sexValue, setSexValue] = useState(['all'])
-  const [eventValue, setEventValue] = useState(['all'])
+  const [sortValue, setSortValue] = useState(['new'])
+  const [sexValue, setSexValue] = useState(FILTERS[1].map(({value})=>value))
+  const [eventValue, setEventValue] = useState(FILTERS[2].map(({value})=>value))
 
-  const handleSexValue = (value) => {
-    setSexValue(value)
+
+  const handleAllChange=(value:boolean,kind:"SEX"|"EVENT")=>{
+    const setValue = kind==="SEX"?setSexValue:setEventValue;
+
+    if(value){
+      const FILTER_INDEX=kind==="SEX"?1:2;
+      const allValue = FILTERS[FILTER_INDEX].map(({value})=>value)
+      setValue(allValue)
+      return;
+    }
+
+    setValue([])
   }
 
-  const handleEventValue = (value) => {
-    setEventValue(value)
+  //TODO: 여기서 필터링해서 클릭/안클릭 해야함
+  const handleSexChange = (value:string[])=>{
+    if(sexValue.includes(value.toString())){
+      setSexValue((sex)=>sex.filter())
+    }
+    console.log(value)
   }
+
+  console.log(sexValue,eventValue,'벨류임')
+
 
   return (
     <>
-      <Chip.Group value={sortValue} onChange={setSortValue}>
+      <Chip.Group multiple onChange={(value)=>setSortValue(value)}>
         {FILTERS[0].map(({ label, value }) => (
           <Chip key={value} value={value} checked={sortValue.includes(value)}>
             {label}
           </Chip>
         ))}
       </Chip.Group>
-      <Chip.Group multiple onChange={handleSexValue}>
+      <Chip value={"all"} checked={sexValue.length===FILTERS[1].length} onChange={(value)=>handleAllChange(value,"SEX")}>전체</Chip>
+      <Chip.Group multiple value={sexValue} onChange={handleSexChange}>
         {FILTERS[1].map(({ label, value }) => (
           <Chip key={value} value={value} checked={sexValue.includes(value)}>
             {label}
           </Chip>
         ))}
       </Chip.Group>
-      <Chip.Group multiple onChange={handleEventValue}>
+      <Chip value={"all"} checked={eventValue.length===FILTERS[2].length} onChange={(value)=>handleAllChange(value,"EVENT")}>전체</Chip>
+      <Chip.Group multiple value={eventValue} onChange={(value)=>setEventValue(value)}>
         {FILTERS[2].map(({ label, value }) => (
           <Chip key={value} value={value} checked={eventValue.includes(value)}>
             {label}
