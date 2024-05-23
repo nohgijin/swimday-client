b52d0d6f6866890485e77f66d3354f1cbd00b0d1
nohgijin <ynda11111@naver.com>
2024-05-23 20:58:22 +0900

    fix: viewport 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,30 @@
-import './layout.scss'
-import localFont from 'next/font/local'
-import { ColorSchemeScript, MantineProvider } from '@mantine/core'
-import '@mantine/core/styles.css'
-import ReactQueryProviders from '@/hooks/useReactQuery'
+import "./layout.scss";
+import localFont from "next/font/local";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import ReactQueryProviders from "@/hooks/useReactQuery";
 
 const font = localFont({
-  src: '../fonts/Pretendard-Regular.woff2',
-  display: 'swap',
-  weight: '45 920',
-})
+  src: "../fonts/Pretendard-Regular.woff2",
+  display: "swap",
+  weight: "45 920",
+});
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={font.className}>
       <head>
         <ColorSchemeScript />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=no;"
+        />
       </head>
       <body>
-          <ReactQueryProviders>
-            <MantineProvider>{children}</MantineProvider>
-          </ReactQueryProviders>
+        <ReactQueryProviders>
+          <MantineProvider>{children}</MantineProvider>
+        </ReactQueryProviders>
       </body>
     </html>
-  )
+  );
 }
