aa1d81c61924414e6b87c73348f66bc0cae77837
nohgijin <ynda11111@naver.com>
2024-07-14 15:00:49 +0900

    markup: 검색 결과 아이템 스타일 수정

--- components/SearchResultItem/index.tsx
+++ components/SearchResultItem/index.tsx
@@ -1,6 +1,6 @@
-"use client";
-import "./style.scss";
-import Link from "next/link";
+'use client'
+import './style.scss'
+import Link from 'next/link'
 
 export type PersonProps = {
   id: number;
@@ -28,26 +28,27 @@ export type TeamProps = {
 
 function TeamSearchResultItem({ id, name, team, ranking, member, gold, silver, bronze }: TeamProps) {
   return (
-    <Link className="result-item" href={`/result/${id}`}>
-      <div className={"team"}>{team}</div>
-      <div className={"name"}>{name}</div>
-      <div className={"result"}>{ranking}위</div>
+    <Link className='result-item' href={`/result/${id}`}>
+      <div className={'team'}>{team}</div>
+      <div className={'name'}>{name}</div>
+      <div className={'result'}>{ranking}위</div>
     </Link>
-  );
+  )
 }
 
 function PersonSearchResultItem({ id, name, team, event, result, ranking, sex, age, isFin, meter }: PersonProps) {
-  const infos = [sex === "male" ? "남자" : "여자", `성인부 ${age}그룹`, `${event} ${meter}m`, isFin ? "핀경기" : ""];
-  const results = `${result} ${ranking === -1 ? "(번외)" : `(${ranking}위)`}`;
+  const infos = [sex === 'male' ? '남자' : '여자', `성인부 ${age}그룹`, `${event} ${meter}m`, isFin && '핀경기']
+  console.log(infos, '정보들')
+  const results = `${result} ${ranking === -1 ? '(번외)' : `(${ranking}위)`}`
 
   return (
-    <Link className="result-item" href={`/result/${id}`}>
-      <div className={"team"}>{team}</div>
-      <div className={"name"}>{name}</div>
-      <div className={"infos"}>{infos.map((info) => info && <div className={"info"}>{info}</div>)}</div>
-      <div className={"result"}>{results}</div>
+    <Link className='result-item' href={`/result/${id}`}>
+      <div className={'team'}>{team}</div>
+      <div className={'name'}>{name}</div>
+      <div className={'infos'}>{infos.map((info) => <div className={'info'}>{info}</div>)}</div>
+      <div className={'result'}>{results}</div>
     </Link>
-  );
+  )
 }
 
-export { TeamSearchResultItem, PersonSearchResultItem };
+export { TeamSearchResultItem, PersonSearchResultItem }
