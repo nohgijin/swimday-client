7824e70db4ffbbb9b03be2169528e5b108bec610
nohgijin <ynda11111@naver.com>
2024-07-30 18:05:41 +0900

    refactor: item->card로 리팩토링

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,50 +1,50 @@
-'use client'
+"use client";
 
-import './style.scss'
-import { ActionIcon } from '@mantine/core'
-import Back from '@/assets/back.svg'
-import { useEffect } from 'react'
-import { ResultFilterGroup } from '@/components/FilterGroup'
-import { useQueryParams } from '@/utils/useQueryParams'
-import { ResultDrawer } from '@/components/Drawer'
-import { useDisclosure } from '@mantine/hooks'
-import { PersonProps, PersonSearchResultItem, TeamProps, TeamSearchResultItem } from '@/components/SearchResultItem'
-import { useRouter } from 'next/navigation'
-import SearchInput from '@/components/SearchInput'
-import { initialState } from '@/store/useChipStore'
+import "./style.scss";
+import { ActionIcon } from "@mantine/core";
+import Back from "@/assets/back.svg";
+import { useEffect } from "react";
+import { ResultFilterGroup } from "@/components/FilterGroup";
+import { useQueryParams } from "@/utils/useQueryParams";
+import { ResultDrawer } from "@/components/Drawer";
+import { useDisclosure } from "@mantine/hooks";
+import { PersonProps, PersonSearchResultCard, TeamProps, TeamSearchResultCard } from "@/components/SearchResultCard";
+import { useRouter } from "next/navigation";
+import SearchInput from "@/components/SearchInput";
+import { initialState } from "@/store/useChipStore";
 
 const PERSON_MOCK_DATA = [
   {
     id: 1,
-    name: '제4회 수원시체육회장배 수영대회 (24.05.19)',
-    team: 'Swimdevil',
-    event: '자유형',
-    result: '35.38',
+    name: "제4회 수원시체육회장배 수영대회 (24.05.19)",
+    team: "Swimdevil",
+    event: "자유형",
+    result: "35.38",
     ranking: 7,
-    sex: 'female',
+    sex: "female",
     age: 1,
     isFin: true,
     meter: 50,
   },
   {
     id: 2,
-    name: '제34회 수원특례시장배 (23.10.11)',
-    team: '어쩌다수영',
-    event: '자유형',
-    result: '37.11',
+    name: "제34회 수원특례시장배 (23.10.11)",
+    team: "어쩌다수영",
+    event: "자유형",
+    result: "37.11",
     ranking: 10,
-    sex: 'female',
+    sex: "female",
     age: 1,
     isFin: false,
     meter: 50,
   },
-]
+];
 
 const TEAM_MOCK_DATA = [
   {
     id: 1,
-    name: '제4회 수원시체육회장배 수영대회 (24.05.19)',
-    team: 'Swimdevil',
+    name: "제4회 수원시체육회장배 수영대회 (24.05.19)",
+    team: "Swimdevil",
     ranking: 7,
     member: 35,
     gold: 3,
@@ -53,51 +53,53 @@ const TEAM_MOCK_DATA = [
   },
   {
     id: 2,
-    name: '제4회 수원시체육회장배 수영대회 (24.05.19)',
-    team: '어쩌다수영',
+    name: "제4회 수원시체육회장배 수영대회 (24.05.19)",
+    team: "어쩌다수영",
     ranking: 7,
     member: 35,
     gold: 3,
     silver: 5,
     bronze: 10,
   },
-]
+];
 
 function Page() {
-  const {
-    queryParams,
-    setQueryParams,
-  } = useQueryParams<{ isTeam: boolean; resultSort: string; gender: string | null; event: string | null }>()
+  const { queryParams, setQueryParams } = useQueryParams<{
+    isTeam: boolean;
+    resultSort: string;
+    gender: string | null;
+    event: string | null;
+  }>();
 
-  const [opened, { open, close }] = useDisclosure(false)
-  const router = useRouter()
-  const isTeam = !!queryParams.get('isTeam')
+  const [opened, { open, close }] = useDisclosure(false);
+  const router = useRouter();
+  const isTeam = !!queryParams.get("isTeam");
 
-  const MOCK_DATA = isTeam ? TEAM_MOCK_DATA : PERSON_MOCK_DATA
+  const MOCK_DATA = isTeam ? TEAM_MOCK_DATA : PERSON_MOCK_DATA;
 
   useEffect(() => {
-    const { resultSort, gender, event } = initialState
+    const { resultSort, gender, event } = initialState;
 
     if (isTeam) {
-      setQueryParams({ resultSort, gender: null, event: null })
-      return
+      setQueryParams({ resultSort, gender: null, event: null });
+      return;
     }
     setQueryParams({
       resultSort,
       gender: gender.toString(),
       event: event.toString(),
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-    <main className={'result-page'}>
-      <div className={'input-wrapper'}>
+    <main className={"result-page"}>
+      <div className={"input-wrapper"}>
         <ActionIcon
-          component={'button'}
-          className={'back'}
-          variant={'transparent'}
+          component={"button"}
+          className={"back"}
+          variant={"transparent"}
           onClick={() => {
-            router.back()
+            router.back();
           }}
         >
           <Back width={24} height={24} />
@@ -106,20 +108,20 @@ function Page() {
       </div>
       <ResultFilterGroup {...{ open }} />
       {opened && <ResultDrawer {...{ opened, close }} />}
-      <div className='results'>
+      <div className="results">
         {MOCK_DATA.map((data) => {
           if (isTeam) {
-            const itemData = data as TeamProps
-            const { id, name, team, ranking, member, gold, silver, bronze } = itemData
-            return <TeamSearchResultItem {...{ id, name, team, ranking, member, gold, silver, bronze }} />
+            const itemData = data as TeamProps;
+            const { id, name, team, ranking, member, gold, silver, bronze } = itemData;
+            return <TeamSearchResultCard {...{ id, name, team, ranking, member, gold, silver, bronze }} />;
           }
-          const itemData = data as PersonProps
-          const { id, name, team, event, result, ranking, sex, age, isFin, meter } = itemData
-          return <PersonSearchResultItem {...{ id, name, team, event, result, ranking, sex, age, isFin, meter }} />
+          const itemData = data as PersonProps;
+          const { id, name, team, event, result, ranking, sex, age, isFin, meter } = itemData;
+          return <PersonSearchResultCard {...{ id, name, team, event, result, ranking, sex, age, isFin, meter }} />;
         })}
       </div>
     </main>
-  )
+  );
 }
 
-export default Page
+export default Page;
