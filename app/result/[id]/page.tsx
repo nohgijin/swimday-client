f8d84d715e3627a1b93318fd07a209d5eee1815a
nohgijin <ynda11111@naver.com>
2024-08-20 15:24:37 +0900

    markup: 개인 경기 결과 디테일 페이지 마크업

--- app/result/[id]/page.tsx
+++ app/result/[id]/page.tsx
@@ -4,6 +4,8 @@ import $ from "./style.module.scss";
 import { ActionIcon } from "@mantine/core";
 import Back from "@/assets/back.svg";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Medal from "@/assets/temp-meal.jpg";
 
 function Page({ params: { id } }: { params: { id: string } }) {
   const router = useRouter();
@@ -15,9 +17,29 @@ function Page({ params: { id } }: { params: { id: string } }) {
           <Back width={24} height={24} />
         </ActionIcon>
       </div>
-      <div className={"result-detail"}>
-        {id}
-        상세는 여기에 들어감
+      <div className={$.detail}>
+        <div className={$.ranking}>1위</div>
+        <div className={$.record}>37.11</div>
+        <div className={$.participant}>
+          노기진
+          <div className={$.team}>팀이름</div>
+        </div>
+        <div className={$.name}>경기이름</div>
+        <div className={$.date}>2024-01-01</div>
+        <div className={$.tags}>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+          <div className={$.tag}>태그1</div>
+        </div>
+        <Image className={$.medal} src={Medal} alt={"메달 이미지"} width={0} height={0} sizes={"100vw"} />
       </div>
     </main>
   );
