0a807e15972aa8dbf59c6a68b437257089984d50
nohgijin <ynda11111@naver.com>
2024-08-20 14:20:36 +0900

    chore: style import 방식 수정

--- app/result/[id]/page.tsx
+++ app/result/[id]/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import './style.scss'
-import { ActionIcon } from '@mantine/core'
-import Back from '@/assets/back.svg'
-import { useRouter } from 'next/navigation'
+import $ from "./style.module.scss";
+import { ActionIcon } from "@mantine/core";
+import Back from "@/assets/back.svg";
+import { useRouter } from "next/navigation";
 
 function Page({ params: { id } }: { params: { id: string } }) {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <main className={"result-detail-page"}>
-      <ActionIcon variant={"transparent"} onClick={() => router.back()}>
-        <Back width={24} height={24} />
-      </ActionIcon>
+    <main className={$["result-detail-page"]}>
+      <div className={$.back}>
+        <ActionIcon variant={"transparent"} onClick={() => router.back()} className={$["back-icon"]}>
+          <Back width={24} height={24} />
+        </ActionIcon>
+      </div>
       <div className={"result-detail"}>
         {id}
         상세는 여기에 들어감
