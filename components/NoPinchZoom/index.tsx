db058622c43d4d1964c1f64dbe53713857330691
nohgijin <ynda11111@naver.com>
2024-09-04 13:17:23 +0900

    fix: touchAction 수정

--- components/NoPinchZoom/index.tsx
+++ components/NoPinchZoom/index.tsx
@@ -4,16 +4,18 @@ import { useEffect } from "react";
 
 function NoPinchZoom() {
   useEffect(() => {
+    document.body.style.touchAction = "pan-x pan-y";
     const listener = (event: TouchEvent) => {
       if (event.touches.length > 1) {
         event.preventDefault();
       }
     };
 
-    document.addEventListener("touchmove", listener, false);
+    document.addEventListener("touchmove", listener, { passive: false });
 
     return () => {
-      document.removeEventListener("touchmove", listener, false);
+      document.body.style.touchAction = "";
+      document.removeEventListener("touchmove", listener);
     };
   }, []);
 
