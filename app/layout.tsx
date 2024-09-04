81374d26a41abe27c672b6d1e87af00af0a1586e
nohgijin <ynda11111@naver.com>
2024-09-04 11:31:43 +0900

    markup: 핀치줌인 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ColorSchemeScript />
         <meta
           name="viewport"
-          content="width=device-width; initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=no;"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
         />
       </head>
-      <body>
+      <body style={{ touchAction: "none" }}>
         <ReactQueryProviders>
           <MantineProvider>{children}</MantineProvider>
         </ReactQueryProviders>
